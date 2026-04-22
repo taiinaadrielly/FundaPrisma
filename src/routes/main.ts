89edbbe5ba473e870314520025c851c9307498fe
Taina Adrielly] <martins.taina@aluno.ifsp.edu.br>
2026-04-22 08:40:17 -0300

    iniciando crud

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -5,3 +5,8 @@ export const mainRouter = Router();
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.post('/user', async (req, res) => {
+    const user = await prisma.user
+}
+)
